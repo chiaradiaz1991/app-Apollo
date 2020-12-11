64b63d8871efba7b399b9b8898aaabe1dbb2c267
chiaradiaz1991 <chiara.diaz1991@gmail.com>
2020-12-11 18:37:12 +0100

    learning apollo with graphql

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const client = new ApolloClient({
+  uri:
+    "https://api-eu-central-1.graphcms.com/v2/ckikhvbwlugjl01xk8ui0dkz4/master",
+  cache: new InMemoryCache(),
+});
+
+const MY_QUERY = gql`
+  {
+    posts {
+      title
+      body
+      createdAt
+      id
+    }
+  }
+`;
+
+client
+  .query({
+    query: MY_QUERY,
+  })
+  .then((res) => console.log(res));
+
+const GetPosts = () => {
+  const { loading, error, data } = useQuery(MY_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.posts.map(({ title, body, id, createdAt }) => (
+    <div className="post-container" key={id}>
+      <h1 className="title">
+        {title}
+      </h1>
+      <p className="content">{body}</p>
+      <p className="createdAt">{createdAt}</p>
     </div>
+  ));
+};
+
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>This is a simple example of Apollo</h1>
+        <GetPosts />
+      </div>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
